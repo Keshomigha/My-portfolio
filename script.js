f894b5e08352bf52317ee25b9922e3ade1cc6811
Keshomigha <keshomighasajeeve@gmail.com>
2025-08-29 16:15:24 +0530

    Modified files

--- script.js
+++ script.js
@@ -9,6 +9,6 @@ document.querySelectorAll(".nav-link").forEach(link => {
 // Animate progress circles
 document.querySelectorAll(".progress-circle").forEach(circle => {
   let value = parseInt(circle.getAttribute("data-value"));
-  let angle = Math.round((value / 100) * 360); // full circle (360°)
+  let angle = Math.round((value / 100) * 360);
   circle.style.background = `conic-gradient(#4b9cdb ${angle}deg, #ddd ${angle}deg)`;
 });
